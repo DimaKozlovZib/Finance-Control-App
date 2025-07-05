f701ba1ac9cf7d8053ddd43a1d9cfb039c4c30cb
Dima Kozlov <zibocinfinity@gmail.com>
2025-07-05 23:14:59 +0300

    настройка корс

--- server/app/main.py
+++ server/app/main.py
@@ -1,22 +1,25 @@
+from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
-
 from app.api.routers import api_router
 from app.core.config import settings
+
 
 def custom_generate_unique_id(route: APIRoute) -> str:
     return f"{route.tags[0] if route.tags else 'Route'}-{route.name}"
 
 
-app = FastAPI(
-    generate_unique_id_function=custom_generate_unique_id,
-)
+app = FastAPI(generate_unique_id_function=custom_generate_unique_id)
+
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*"],
     allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
